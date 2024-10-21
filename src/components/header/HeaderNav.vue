c1466deb5c1be991a2f30238fb94185e169d6f59
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2024-10-21 00:41:19 -0600

    layout base

---
+++ src/components/header/HeaderNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-app-bar :elevation="8" rounded>
+      <template v-slot:prepend>
+      </template>
+      <v-app-bar-title><b>Portafolio</b> Ade Cuellar </v-app-bar-title>
+
+      <template v-slot:append>
+        <v-btn class="ma-3">
+          Guardar y ver
+          <v-btn icon="mdi-content-save-check"></v-btn>
+        </v-btn>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+    </v-app-bar>
+  </v-app>
+</template>
+<script setup>
+
+</script>
---
+++ src/components/slide/SlideLeft.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-navigation-drawer>
+      <v-list-item title="Componentes" subtitle="Lista"></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item link title="List Item 1"></v-list-item>
+      <v-list-item link title="List Item 2"></v-list-item>
+      <v-list-item link title="List Item 3"></v-list-item>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+<script setup>
+
+</script>
